581837b10ec7dc014e3be67e867dea9054e24042
schuetz-dhbw <75082495+schuetz-dhbw@users.noreply.github.com>
2025-11-11 15:15:22 +0100

    todo list with items

--- app/src/main/java/de/dhbw/heidenheim/schuetz/todoapp/MainActivity.kt
+++ app/src/main/java/de/dhbw/heidenheim/schuetz/todoapp/MainActivity.kt
@@ -1,23 +1,18 @@
 package de.dhbw.heidenheim.schuetz.todoapp
 
 import android.os.Bundle
-import android.util.Log.i
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -39,23 +34,25 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+
 @Preview (showBackground = true)
 @Composable
 fun ToDoScreen(modifier: Modifier = Modifier) {
+
+    val todos = listOf("Einkaufen", "Lernen", "Auto waschen", "Projekt bearbeiten")
+
     LazyColumn(
         modifier = modifier
             .fillMaxSize()
     ) {
-        items(count = 500) {
-            Text(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(vertical = 24.dp),
-                text = "Item $it",
-                fontSize = 24.sp,
-                fontWeight = FontWeight.Bold,
-                textAlign = TextAlign.Center
-            )
+        items(items = todos) { todo ->
+                Text(
+                    text = todo,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp),
+                    fontSize = 24.sp
+                )
+            }
         }
     }
-}
